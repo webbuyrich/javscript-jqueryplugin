b92cafea7ad50206a39e4baec69e485043d84ace
Richard Peterson <webbuyrich@gmail.com>
2016-01-14 14:38:54 -0600

    added carousel

--- js/main.js
+++ js/main.js
@@ -1,38 +1,46 @@
 $(document).ready(function(){
 
-// Animation Transistions
-$('.animsition').animsition({
+	// Animation Transistions
+	$('.animsition').animsition({
 
-	inClass: 'fade-in-right-lg',
-	outClass: 'fade-out-right-lg',
-	linkElements: '.sport-link'
+		inClass: 'fade-in-right-lg',
+		outClass: 'fade-out-right-lg',
+		linkElements: '.sport-link'
 
-});
+	});
+		
+	// Add Static Top Nav
+	$('.blog-nav').sticky();
+
+	/*************** SHOWING AND REMOVING STICKY TEXT **********************/
+	//show text when sticky
+	$('.blog-nav').on('sticky-start', function(){
+		$('.description').html('The color changes when you begin to scroll');
+
+	});
+	//remove text when sticky
+	$('.blog-nav').on('sticky-end', function(){
+		$('.description').html('');
+	});
+
+
+	$('.blog-description').sticky({topSpacing:50, padding: 20});
+	$('.blog-description').on('sticky-start', function(){
+		$(this).append(' <a href="view-source:http://getbootstrap.com/examples/blog/" class="nav-link"> View Source</a>');
+	});
+	$('.blog-description').on('sticky-end', function(){
+		$('.nav-link').remove();
+	});
+
+
+
+	/*************** MAKING A CAROUSEL **********************/
+	$('.slides').slick({
+		arrows: false,	 
+		dots: true, 
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 2000	
+	});
 	
-// Add Static Top Nav
-$('.blog-nav').sticky({
-
-});
-
-/*************** SHOWING AND REMOVING STICKY TEXT **********************/
-//show text when sticky
-$('.blog-nav').on('sticky-start', function(){
-	$('.description').html('The color changes when you begin to scroll');
-
-});
-//remove text when sticky
-$('.blog-nav').on('sticky-end', function(){
-	$('.description').html('');
-});
-
-
-$('.blog-description').sticky({topSpacing:50, padding: 20});
-$('.blog-description').on('sticky-start', function(){
-	$(this).append(' <a href="view-source:http://getbootstrap.com/examples/blog/" class="nav-link"> View Source</a>');
-});
-$('.blog-description').on('sticky-end', function(){
-	$('.nav-link').remove();
-});
-
-
 });
