1806e4355d37d6a26d7847cb5f2a4fb9b8e5d755
Richard Peterson <webbuyrich@gmail.com>
2016-01-14 13:20:36 -0600

    added second nav on stick. animate blog heading

--- js/main.js
+++ js/main.js
@@ -14,11 +14,25 @@ $('.blog-nav').sticky({
 
 });
 
+/*************** SHOWING AND REMOVING STICKY TEXT **********************/
+//show text when sticky
 $('.blog-nav').on('sticky-start', function(){
 	$('.description').html('The color changes when you begin to scroll');
+
 });
+//remove text when sticky
 $('.blog-nav').on('sticky-end', function(){
 	$('.description').html('');
 });
+
+
+$('.blog-description').sticky({topSpacing:50, padding: 20});
+$('.blog-description').on('sticky-start', function(){
+	$(this).append(' <a href="view-source:http://getbootstrap.com/examples/blog/" class="nav-link"> View Source</a>');
+});
+$('.blog-description').on('sticky-end', function(){
+	$('.nav-link').remove();
+});
+
 
 });
